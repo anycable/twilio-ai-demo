e2ed3037ed0d1f7019d0967f3c71719183c67b82
Vladimir Dementyev <dementiev.vm@gmail.com>
2024-11-01 10:45:59 -0700

    + db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Generate some example todos
+
+Todo.create([
+  {description: "Read the AnyCable off Rails blog post", deadline: 1.week.ago},
+  {description: "Create a new Rails demo app", deadline: 5.days.ago, completed_at: 5.days.ago},
+  {description: "Figure out how to convert PCM 24kHz to mulaw 8kHz in Ruby", deadline: 3.days.ago, completed_at: 3.days.ago},
+  {description: "Write a blog post about AnyCable, Twilio and OpenAI", deadline: Date.current, completed: true},
+  {description: "Deploy application to Fly.io", deadline: 2.days.since},
+  # Some future tasks
+  {description: "Upgrade to Ruby 3.#{RUBY_VERSION.split(".")[1].to_i.succ}", deadline: "#{Date.current.year}-12-25"}
+])
